ea7bcb4d11558cdf8c6b728cba2b1fa72969bba7
unknown <robson.luizv@gmail.com>
2014-02-22 18:21:10 -0300

    Criando método para geração randomica de senhas fortes. Implementação falsa.

--- src/service/StrongPasswordService.java
+++ src/service/StrongPasswordService.java
@@ -3,11 +3,19 @@ package service;
 import static type.PasswordStreghtType.MEDIUM;
 import static type.PasswordStreghtType.STRONG;
 import static type.PasswordStreghtType.WEAK;
+
+import java.util.Random;
+
 import type.PasswordStreghtType;
 import br.com.caelum.vraptor.ioc.Component;
 
 @Component
 public class StrongPasswordService {
+	
+	
+	public String generateRandomStrongPassword() {
+		return "nwjh8wubc4huu2wsch6nnxk";
+	}
 	
 	public PasswordStreghtType verifyPasswordStrenght(String password) {
 		if (isStrongPassword(password))
