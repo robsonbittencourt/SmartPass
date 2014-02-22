7665d445410113122562ff18f9429b540aa929c5
unknown <robson.luizv@gmail.com>
2014-02-22 12:50:56 -0300

    Criando model de password

---
+++ src/model/Password.java
@@ -0,0 +1,24 @@
+package model;
+
+
+public class Password {
+	
+	private String password;
+	private String status;
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
