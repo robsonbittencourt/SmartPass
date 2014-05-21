673ca95acb1c8ffd5751f46f7849fec818609eb7
unknown <robson.luizv@gmail.com>
2014-05-21 00:01:31 -0300

    Adicionando método de encriptação RSA

---
+++ src/encryption/EncryptionUtil.java
@@ -0,0 +1,86 @@
+// Adapting for: http://javadigest.wordpress.com/2012/08/26/rsa-encryption-example/
+
+package encryption;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.Cipher;
+
+public class EncryptionUtil {
+
+	public static final String ALGORITHM = "RSA";
+	public static final String PRIVATE_KEY_FILE = "C:/keys/private.key";
+	public static final String PUBLIC_KEY_FILE = "C:/keys/public.key";
+
+	public void generateKey() {
+		try {
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
+			keyGen.initialize(1024);
+			KeyPair key = keyGen.generateKeyPair();
+
+			File privateKeyFile = new File(PRIVATE_KEY_FILE);
+			File publicKeyFile = new File(PUBLIC_KEY_FILE);
+
+			if (privateKeyFile.getParentFile() != null) {
+				privateKeyFile.getParentFile().mkdirs();
+			}
+			privateKeyFile.createNewFile();
+
+			if (publicKeyFile.getParentFile() != null) {
+				publicKeyFile.getParentFile().mkdirs();
+			}
+			publicKeyFile.createNewFile();
+
+			ObjectOutputStream publicKeyOS = new ObjectOutputStream(new FileOutputStream(publicKeyFile));
+			publicKeyOS.writeObject(key.getPublic());
+			publicKeyOS.close();
+
+			ObjectOutputStream privateKeyOS = new ObjectOutputStream(new FileOutputStream(privateKeyFile));
+			privateKeyOS.writeObject(key.getPrivate());
+			privateKeyOS.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public boolean areKeysPresent() {
+		File privateKey = new File(PRIVATE_KEY_FILE);
+		File publicKey = new File(PUBLIC_KEY_FILE);
+
+		if (privateKey.exists() && publicKey.exists()) {
+			return true;
+		}
+		return false;
+	}
+
+	public byte[] encrypt(String text, PublicKey key) {
+		byte[] cipherText = null;
+		try {
+			Cipher cipher = Cipher.getInstance(ALGORITHM);
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			cipherText = cipher.doFinal(text.getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return cipherText;
+	}
+
+	public String decrypt(byte[] text, PrivateKey key) {
+		byte[] dectyptedText = null;
+		try {
+			Cipher cipher = Cipher.getInstance(ALGORITHM);
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			dectyptedText = cipher.doFinal(text);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return new String(dectyptedText);
+	}
+
+}
